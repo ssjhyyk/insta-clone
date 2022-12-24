3ecab04c41ce6e52f406a298b5c079b0f73b193a
Vaishnavi <112963608+ssjhyyk@users.noreply.github.com>
2022-12-25 03:09:07 +0530

    Co-authored-by: KANISHAK CHAURASIA <kanishak11-cyber@users.noreply.github.com>

---
+++ components/Feed.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Stories from "./Stories"
+
+function feed() {
+  return (
+    <main className='grid grid-cols-1 md-grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl max-auto'>
+        <div className='col-span-2'>
+            <section>
+                {/* Stories */}
+                <Stories/>
+                {/* Posts */}
+            </section>
+            
+            <section>
+                {/* mini profile */}
+                {/* Suggestions */}
+            </section>
+
+        </div>
+    </main>
+  )
+}
+
+export default feed
